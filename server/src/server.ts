1211ed835e62126e02cb104db820e44a094e0479
ThiagoHonoP <thiagopedrosa.thp@gmail.com>
2024-09-10 15:16:30 -0300

    feat: add variables

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
+import "dotenv/config";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server listening on port 3000123 ${port}`));
+app.listen(port, () => console.log(`Server listening on port ${port}`));
