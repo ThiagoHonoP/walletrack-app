ae870999bed9a044884b1416d2078e97da0d930f
ThiagoHonoP <thiagopedrosa.thp@gmail.com>
2024-09-11 14:57:02 -0300

    feat: add schema SignUp user

---
+++ server/src/schemas/User.ts
@@ -0,0 +1,24 @@
+import mongoose, { model, Schema } from "mongoose";
+import { ISignupForm } from "../interfaces/IAuth";
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export default model<ISignupForm>("users", userSchema);
