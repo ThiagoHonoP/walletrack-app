ecf17bb55673472cde1d83ac95455aa62d33a827
ThiagoHonoP <thiagopedrosa.thp@gmail.com>
2024-09-17 13:20:28 -0300

    feat: check userExists Repository

--- server/src/repositories/authRepository.ts
+++ server/src/repositories/authRepository.ts
@@ -1,7 +1,17 @@
-const create = (data: any) => {
-  return data;
+import UserSchema from "../schemas/User";
+
+const create = async (data: any) => {
+  return await UserSchema.create(data);
+};
+
+const findByEmail = async (email: string) => {
+  const user = await UserSchema.findOne({
+    email,
+  });
+  return user;
 };
 
 export default {
   create,
+  findByEmail,
 };
