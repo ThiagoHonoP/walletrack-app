a2e51e7f61bf7031b7c057696e631b77c8d3d5f1
ThiagoHonoP <thiagopedrosa.thp@gmail.com>
2024-10-07 12:24:16 -0300

    feat: add auth pages Signin and Signup

--- web/src/pages/Signin/index.tsx
+++ web/src/pages/Signin/index.tsx
@@ -2,18 +2,44 @@ import { Link } from "react-router-dom";
 import { Box } from "../../components/Box";
 import { PrimaryButton } from "../../components/Button";
 import { Input } from "../../components/Input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signinSchema } from "../../schemas/Signin";
 
 const Signin = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(signinSchema),
+  });
+
+  function handleSubmitForm(data: any) {
+    console.log(data);
+  }
+
   return (
-    <main className="bg-teal-300 flex justify-center items-center h-screen">
+    <main className="bg-black flex justify-center items-center h-screen">
       <Box>
         <div className="flex justify-between">
           <h1> Login </h1>
           <p> img </p>
         </div>
-        <form>
-          <Input type="email" placeholder="Enter your email" />
-          <Input type="password" placeholder="Enter your password" />
+        <form onSubmit={handleSubmit(handleSubmitForm)}>
+          <Input
+            name="email"
+            type="email"
+            placeholder="Enter your email"
+            register={register("email")}
+          />
+          {errors.email && <span>{errors.root?.message}</span>}
+          <Input
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+            register={register("password")}
+          />
           <PrimaryButton text="Login" type="submit" />
         </form>
         <Link to="/signup"> Don't have account ? Signup </Link>
--- web/src/pages/Signup/index.tsx
+++ web/src/pages/Signup/index.tsx
@@ -2,22 +2,63 @@ import { Link } from "react-router-dom";
 import { Box } from "../../components/Box";
 import { PrimaryButton } from "../../components/Button";
 import { Input } from "../../components/Input";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { signupSchema } from "../../schemas/signup";
 
 const Signup = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(signupSchema),
+  });
+
+  function handleSubmitForm(data: any) {
+    console.log(data);
+  }
+
   return (
-    <main className="bg-teal-300 flex justify-center items-center h-screen">
+    <main className="bg-black flex justify-center items-center h-screen">
       <Box>
         <div className="flex justify-between">
           <h1> Signup </h1>
-          <Link to="/signin"> Voltar </Link>
+          <p> img </p>
         </div>
-        <form>
-          <Input type="text" placeholder="Full name" />
-          <Input type="email" placeholder="Enter your email" />
-          <Input type="password" placeholder="Enter your password" />
-          <Input type="password" placeholder="Confirm password" />
+        <form onSubmit={handleSubmit(handleSubmitForm)}>
+          <Input
+            type="name"
+            name="name"
+            placeholder="Full name"
+            register={register("name")}
+          />
+          {errors.name && <span>{errors.name?.message}</span>}
+          <Input
+            name="email"
+            type="email"
+            placeholder="Enter your email"
+            register={register("email")}
+          />
+          {errors.email && <span>{errors.email?.message}</span>}
+          <Input
+            name="password"
+            type="password"
+            placeholder="Enter your password"
+            register={register("password")}
+          />
+          {errors.password && <span>{errors.password?.message}</span>}
+          <Input
+            name="confirmPassword"
+            type="password"
+            placeholder="Confirm password"
+          />
+          {errors.confirmPassword && (
+            <span>{errors.confirmPassword?.message}</span>
+          )}
           <PrimaryButton text="Signup" type="submit" />
         </form>
+        <Link to="/signin"> Voltar </Link>
       </Box>
     </main>
   );
