f42c2ff23b769ac20a995f57b175d7d4969d0b8f
ThiagoHonoP <thiagopedrosa.thp@gmail.com>
2024-10-07 16:04:22 -0300

    chore: enable with the cors package backend side

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,12 +3,14 @@ import "dotenv/config";
 import { connectDb } from "./config/database";
 import authRouter from "./routes/authRoutes";
 import transactionRouter from "./routes/transactionRoutes";
+import cors from "cors";
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 connectDb();
 app.use(json());
+app.use(cors());
 app.use(authRouter);
 app.use(transactionRouter);
 
